c761cf602b4c285b8460ecff3d9eff96bf3ddab9
jibLord <155445673+jibLord@users.noreply.github.com>
2024-05-27 09:59:29 +0200

    First push for team ml_models

---
+++ datasets/admin.py
@@ -0,0 +1,11 @@
+admin.site.register(Datasets)
+
+from datasets.models import Dataset
+
+admin.site.register(Dataset)
+
+
+# admin.site
+# from django.contrib import admin
+
+# Register your datasets here.
--- views/admin.py
+++ views/admin.py
@@ -1,7 +1,3 @@
 from django.contrib import admin
-from .models import Prompt
-
 
 # Register your datasets here.
-admin.site.register(Prompt)
-
