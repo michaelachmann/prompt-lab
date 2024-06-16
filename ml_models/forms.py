a577ae3a1f0cac9201c3436040f4288fdaffe0ce
jibLord <155445673+jibLord@users.noreply.github.com>
2024-06-16 14:08:33 +0200

    updated a few things so that everything runs smoothly

--- ml_models/forms.py
+++ ml_models/forms.py
@@ -4,4 +4,4 @@ from .models import MLModel
 class MLModelForm(forms.ModelForm):
     class Meta:
         model = MLModel
-        fields = ['name', 'type', 'description', 'tags', 'parameters', 'size', 'version']  # Fügen Sie weitere Felder nach Bedarf hinzu
+        fields = ['name', 'type', 'description', 'tags', 'parameters', 'size', 'version', 'rating']  # Fügen Sie weitere Felder nach Bedarf hinzu
--- ml_models/urls.py
+++ ml_models/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.ml_model_list, name='ml_model_list'),
     path('ml_model/new', views.ml_model_create, name='ml_model_create'),
+    path('<int:pk>/', views.ml_model_detail, name='ml_model_detail'),
     path('ml_model/<int:pk>/edit/', views.ml_model_update, name='ml_model_update'),
     path('ml_model/<int:pk>/delete/', views.ml_model_delete, name='ml_model_delete'),
 ]
