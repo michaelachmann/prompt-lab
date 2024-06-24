50dec56132fe6a44b42475cc66a1c728435e1188
jibLord <155445673+jibLord@users.noreply.github.com>
2024-06-24 09:11:12 +0200

    Final Changes

--- MLmodels/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Prompt
-
-class MLmodelForm(forms.ModelForm):
-    class Meta:
-        model = MLmodel
-        fields = ['name', 'type', 'description', 'tags', 'parameters', 'size', 'version']  # Fügen Sie weitere Felder nach Bedarf hinzu
